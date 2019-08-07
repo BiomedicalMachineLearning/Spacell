0dd5612ce80034dc361edf1dda8ad105beed6fb4
xiao233333 <43783271+xiao233333@users.noreply.github.com>
2019-08-07 15:22:23 +1000

    version 0.9.9

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 
 setuptools.setup(
-    name="SpaCell",
-    version="0.0.1",
+    name="Spacell",
+    version="0.9.9",
     author="Xiao Tan, Andrew Su, Quan Nguyen",
     author_email="xiao.tan@uq.edu.au, a.su@uq.net.au, quan.nguyen@imb.uq.edu.au",
-    description="SpaCell Package",
+    description="Spacell Package",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/BiomedicalMachineLearning/Spacell.git",
